6818d6288deaf4d7505665f8b79a2c6d10fa1cd1
Fayjul Islam Al Imran <fayjulislamalimran7@gmail.com>
2022-10-12 22:08:47 +0600

    Quiz  Color Fix

--- src/Components/Option/Option.jsx
+++ src/Components/Option/Option.jsx
@@ -13,7 +13,7 @@ const Option = ({ option, correctAnswer }) => {
   return (
     <div className="mt-3">
       <div className="bg-green-500 text-black flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-        <input onClick={handleCheck} defaultChecked id={option} type="radio" defaultValue name="bordered-radio" className="w-4 h-4 text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600  dark:border-gray-600" />
+        <input onClick={handleCheck} defaultChecked id={option} type="radio" defaultValue name="bordered-radio" className="w-4 h-4 text-blue-600 focus:ring-blue-400 dark:focus:ring-blue-600  dark:border-gray-600" />
         <label htmlFor={option} className="py-4 ml-2 w-full text-gray-900">
           {1 && option}
         </label>
